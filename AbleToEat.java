f493bd79a250f7a8f2c47b18a46d3dd3658a5cdb
Bulgakoff <hlbu@yandex.ru>
2019-08-17 08:59:34 +0300

    добавили интерфейс

--- AbleToEat.java
+++ AbleToEat.java
@@ -1,4 +1,19 @@
 package AlishevAnonimClassLess35;
 
-public interface AbleToEat {
+ interface AbleToEat {
+    public void eat();
+}
+
+class Test {
+    public static void main(String[] args) {
+        AbleToEat ableToEat = new AbleToEat() {
+            @Override
+            public void eat() {
+                System.out.println(" Some one eating  ");
+
+            }
+        };//объект этого класса создан только на раз , на обно использование
+        ableToEat.eat();
+    }
+
 }
